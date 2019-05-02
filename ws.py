3b904b4396084915f88e5245410afd90b07f29dd
Muhamad Yusup Hamdani <dani.webdev@gmail.com>
2019-05-02 15:11:47 +0700

    update

--- ws.py
+++ ws.py
@@ -1,4 +1,4 @@
-
+import pyttsx3
 import time
 from urllib.parse import unquote
 from bs4 import BeautifulSoup
@@ -6,6 +6,12 @@ from subprocess import PIPE, Popen
 import re, sys
 
 q = sys.argv[1]
+print(q)
+engine = pyttsx3.init()
+engine.setProperty('voice', "indonesia")
+engine.setProperty('rate', 150)
+engine.setProperty('volume', 0.9)
+engine.say('Hallo dani, system sedang mencari dengan kata {}...'.format(q));
 
 process = Popen(
     args="curl -s https://duckduckgo.com/html/?q={}".format(q),
@@ -31,12 +37,18 @@ for i in range(0, len(get_element)-1): #'a' tags are for links
     data.append(desc_element.text)
     output.append(data)
 
+engine.say('Total result {}'.format(len(output)));
+engine.runAndWait()
+
 for i, data in enumerate(output):
     print("No : {}".format(i+1))
     print("JUDUL : {}".format(data[0]))
     print("LINK : {}".format(data[1]))
     print("DESC : {}".format(data[2]))
     print("")
+    engine.say('{}'.format(data[0]));
+    engine.runAndWait()
+
     with open("output.txt", "a") as myfile:
         myfile.write("No : {}\n".format(i+1))
         myfile.write("JUDUL : {}\n".format(data[0]))
@@ -45,3 +57,4 @@ for i, data in enumerate(output):
         myfile.write("\n")
         
     time.sleep(1)
+
