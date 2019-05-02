b27fd1ac053c2e3c09b0de28a1ce920bd6aa0d34
Muhamad Yusup Hamdani <dani.webdev@gmail.com>
2019-05-02 12:10:15 +0700

    first commit

---
+++ ws.py
@@ -0,0 +1,47 @@
+
+import time
+from urllib.parse import unquote
+from bs4 import BeautifulSoup
+from subprocess import PIPE, Popen
+import re, sys
+
+q = sys.argv[1]
+
+process = Popen(
+    args="curl -s https://duckduckgo.com/html/?q={}".format(q),
+    stdout=PIPE,
+    shell=True
+)
+out = process.communicate()[0]
+
+# Set the URL you want to webscrape from
+soup        = BeautifulSoup(out, "html.parser")
+get_element = soup.findAll("div", {"class": "links_main links_deep result__body"})
+output      = []
+
+# # # To download the whole data set, let's do a for loop through all a tags
+for i in range(0, len(get_element)-1): #'a' tags are for links
+    one_a_tag   = get_element[i]
+    sub_element = one_a_tag.find("a")
+    desc_element= one_a_tag.find('a', {"class": "result__snippet"})
+
+    data          = []
+    data.append(sub_element.text)
+    data.append(unquote(sub_element['href']).split("uddg=")[1])
+    data.append(desc_element.text)
+    output.append(data)
+
+for i, data in enumerate(output):
+    print("No : {}".format(i+1))
+    print("JUDUL : {}".format(data[0]))
+    print("LINK : {}".format(data[1]))
+    print("DESC : {}".format(data[2]))
+    print("")
+    with open("output.txt", "a") as myfile:
+        myfile.write("No : {}\n".format(i+1))
+        myfile.write("JUDUL : {}\n".format(data[0]))
+        myfile.write("LINK : {}\n".format(data[1]))
+        myfile.write("DESC : {}\n".format(data[2]))
+        myfile.write("\n")
+        
+    time.sleep(1)
